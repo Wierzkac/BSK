c942a78f944afbccfc0b29095fe77f8f6052620b
Wierzkac <wierzkac@gmail.com>
2019-03-02 21:20:33 +0100

    Wczytywanie i Zapisywanie pliku

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.WybierzPlik = new System.Windows.Forms.Button();
+            this.ZapiszJako = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // WybierzPlik
+            // 
+            this.WybierzPlik.Location = new System.Drawing.Point(74, 173);
+            this.WybierzPlik.Name = "WybierzPlik";
+            this.WybierzPlik.Size = new System.Drawing.Size(130, 48);
+            this.WybierzPlik.TabIndex = 0;
+            this.WybierzPlik.Text = "Wybierz plik do zaszyfrowania";
+            this.WybierzPlik.UseVisualStyleBackColor = true;
+            this.WybierzPlik.Click += new System.EventHandler(this.WybierzPlik_Click);
+            // 
+            // ZapiszJako
+            // 
+            this.ZapiszJako.Location = new System.Drawing.Point(524, 185);
+            this.ZapiszJako.Name = "ZapiszJako";
+            this.ZapiszJako.Size = new System.Drawing.Size(143, 36);
+            this.ZapiszJako.TabIndex = 1;
+            this.ZapiszJako.Text = "Zapisz plik jako...";
+            this.ZapiszJako.UseVisualStyleBackColor = true;
+            this.ZapiszJako.Click += new System.EventHandler(this.ZapiszJako_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ZapiszJako);
+            this.Controls.Add(this.WybierzPlik);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button WybierzPlik;
+        private System.Windows.Forms.Button ZapiszJako;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,50 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        private OpenFileDialog openFileDialog1;
+        private byte[] fileData = null;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void WybierzPlik_Click(object sender, EventArgs e)
+        {
+            openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var filePath = openFileDialog1.FileName;
+                byte[] plik = FileToByteArray(openFileDialog1.FileName);    
+            }
+        }
+        private byte[] FileToByteArray(string fileName)
+        {
+            using (FileStream fs = File.OpenRead(fileName))
+            {
+                using (BinaryReader binaryReader = new BinaryReader(fs))
+                {
+                    fileData = binaryReader.ReadBytes((int)fs.Length);
+                }
+            }
+            return fileData;
+        }
+        private void ByteArrayToFile(byte[] array)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.ShowDialog();
+
+            FileStream fs =(System.IO.FileStream)saveFileDialog.OpenFile();
+            fs.Close();
+
+            File.WriteAllBytes(fs.Name, array);
+
+
+        }
+
+        private void ZapiszJako_Click(object sender, EventArgs e)
+        {
+            ByteArrayToFile(fileData);
         }
     }
 }
