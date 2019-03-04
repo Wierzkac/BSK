553e0df01cd73726f48047401107d2194b283935
Wierzkac <wierzkac@gmail.com>
2019-03-04 15:57:33 +0100

    zalazek sieci

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -30,11 +30,13 @@
         {
             this.WybierzPlik = new System.Windows.Forms.Button();
             this.ZapiszJako = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // WybierzPlik
             // 
-            this.WybierzPlik.Location = new System.Drawing.Point(74, 173);
+            this.WybierzPlik.Location = new System.Drawing.Point(95, 12);
             this.WybierzPlik.Name = "WybierzPlik";
             this.WybierzPlik.Size = new System.Drawing.Size(130, 48);
             this.WybierzPlik.TabIndex = 0;
@@ -44,24 +46,46 @@
             // 
             // ZapiszJako
             // 
-            this.ZapiszJako.Location = new System.Drawing.Point(524, 185);
+            this.ZapiszJako.Location = new System.Drawing.Point(95, 402);
             this.ZapiszJako.Name = "ZapiszJako";
-            this.ZapiszJako.Size = new System.Drawing.Size(143, 36);
+            this.ZapiszJako.Size = new System.Drawing.Size(130, 36);
             this.ZapiszJako.TabIndex = 1;
             this.ZapiszJako.Text = "Zapisz plik jako...";
             this.ZapiszJako.UseVisualStyleBackColor = true;
             this.ZapiszJako.Click += new System.EventHandler(this.ZapiszJako_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Wczytałeś plik:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 110);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(307, 22);
+            this.textBox1.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(331, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.ZapiszJako);
             this.Controls.Add(this.WybierzPlik);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +93,8 @@
 
         private System.Windows.Forms.Button WybierzPlik;
         private System.Windows.Forms.Button ZapiszJako;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,7 +7,12 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 using System.Windows.Forms;
+using static AsynchronousSocketListener;
 
 namespace WindowsFormsApp1
 {
@@ -19,6 +24,12 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+
+                StartListening();
+            }).Start();
         }
 
         private void WybierzPlik_Click(object sender, EventArgs e)
@@ -26,11 +37,22 @@ namespace WindowsFormsApp1
             openFileDialog1 = new OpenFileDialog();
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                var filePath = openFileDialog1.FileName;
-                byte[] plik = FileToByteArray(openFileDialog1.FileName);    
+                FileInfo fi = new FileInfo(openFileDialog1.FileName);
+                long fileSize = fi.Length;
+                if (fileSize > 104857600 || fileSize < 1024) {
+                    MessageBox.Show("Twój plik jest albo poniżej 1kB albo powyżej 100MB!\n Wybierz ponownie plik.",
+                    "ERROR",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                }
+                else {
+                    var filePath = openFileDialog1.FileName;
+                    FileToByteArray(openFileDialog1.FileName);
+                    textBox1.Text = openFileDialog1.SafeFileName;
+                }
             }
         }
-        private byte[] FileToByteArray(string fileName)
+        private void FileToByteArray(string fileName)
         {
             using (FileStream fs = File.OpenRead(fileName))
             {
@@ -39,24 +61,31 @@ namespace WindowsFormsApp1
                     fileData = binaryReader.ReadBytes((int)fs.Length);
                 }
             }
-            return fileData;
         }
         private void ByteArrayToFile(byte[] array)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "txt files (*.txt)|*.txt|mp3 files (*.mp3)|*.mp3|png files (*.png)|*.png|avi files (*.avi)|*.avi|jpg files (*.jpg)|*.jpg|All files (*.*)|*.*";
             saveFileDialog.ShowDialog();
 
             FileStream fs =(System.IO.FileStream)saveFileDialog.OpenFile();
             fs.Close();
-
             File.WriteAllBytes(fs.Name, array);
-
-
         }
 
         private void ZapiszJako_Click(object sender, EventArgs e)
         {
             ByteArrayToFile(fileData);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
