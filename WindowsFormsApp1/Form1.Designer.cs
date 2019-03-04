85bdf4c01c7973cf49a59b8c3628a5b24c1e52f6
jakublatkowski <jakublatkowski21@gmail.com>
2019-03-04 21:35:13 +0100

    Update Form1.Designer.cs
    Dodałem nazwę na barze głównym

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -36,10 +36,10 @@ namespace WindowsFormsApp1
             // 
             // WybierzPlik
             // 
-            this.WybierzPlik.Location = new System.Drawing.Point(52, 25);
-            this.WybierzPlik.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.WybierzPlik.Location = new System.Drawing.Point(39, 20);
+            this.WybierzPlik.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.WybierzPlik.Name = "WybierzPlik";
-            this.WybierzPlik.Size = new System.Drawing.Size(131, 48);
+            this.WybierzPlik.Size = new System.Drawing.Size(98, 39);
             this.WybierzPlik.TabIndex = 0;
             this.WybierzPlik.Text = "Wybierz plik do zaszyfrowania";
             this.WybierzPlik.UseVisualStyleBackColor = true;
@@ -47,10 +47,10 @@ namespace WindowsFormsApp1
             // 
             // ZapiszJako
             // 
-            this.ZapiszJako.Location = new System.Drawing.Point(127, 495);
-            this.ZapiszJako.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ZapiszJako.Location = new System.Drawing.Point(95, 402);
+            this.ZapiszJako.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ZapiszJako.Name = "ZapiszJako";
-            this.ZapiszJako.Size = new System.Drawing.Size(173, 44);
+            this.ZapiszJako.Size = new System.Drawing.Size(130, 36);
             this.ZapiszJako.TabIndex = 1;
             this.ZapiszJako.Text = "Zapisz plik jako...";
             this.ZapiszJako.UseVisualStyleBackColor = true;
@@ -64,28 +64,25 @@ namespace WindowsFormsApp1
             "CBC",
             "CFB",
             "OFB "});
-            this.encodingModeComboBox.Location = new System.Drawing.Point(247, 49);
-            this.encodingModeComboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.encodingModeComboBox.Location = new System.Drawing.Point(185, 40);
             this.encodingModeComboBox.Name = "encodingModeComboBox";
-            this.encodingModeComboBox.Size = new System.Drawing.Size(160, 24);
+            this.encodingModeComboBox.Size = new System.Drawing.Size(121, 21);
             this.encodingModeComboBox.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(244, 25);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(183, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(165, 17);
+            this.label1.Size = new System.Drawing.Size(123, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Wybierz tryb szyfrowania";
             // 
             // startButton
             // 
-            this.startButton.Location = new System.Drawing.Point(329, 213);
-            this.startButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.startButton.Location = new System.Drawing.Point(247, 173);
             this.startButton.Name = "startButton";
-            this.startButton.Size = new System.Drawing.Size(100, 28);
+            this.startButton.Size = new System.Drawing.Size(75, 23);
             this.startButton.TabIndex = 4;
             this.startButton.Text = "Rozpocznij";
             this.startButton.UseVisualStyleBackColor = true;
@@ -100,10 +97,9 @@ namespace WindowsFormsApp1
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 100);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(12, 81);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(101, 17);
+            this.label2.Size = new System.Drawing.Size(80, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Wczytałeś plik:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -111,17 +107,16 @@ namespace WindowsFormsApp1
             // textBox1
             // 
             this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(16, 135);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Location = new System.Drawing.Point(12, 110);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(408, 22);
+            this.textBox1.Size = new System.Drawing.Size(307, 20);
             this.textBox1.TabIndex = 3;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(441, 554);
+            this.ClientSize = new System.Drawing.Size(331, 450);
             this.Controls.Add(this.startButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.encodingModeComboBox);
@@ -129,9 +124,9 @@ namespace WindowsFormsApp1
             this.Controls.Add(this.label2);
             this.Controls.Add(this.ZapiszJako);
             this.Controls.Add(this.WybierzPlik);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "SuperEncoder";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
             this.ResumeLayout(false);
