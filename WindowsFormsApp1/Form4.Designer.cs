86d97d4d5848df441bb8fc660ddc8db81ca8977b
jakublatkowski <jakublatkowski21@gmail.com>
2019-03-08 10:53:30 +0100

    UI i funkcjonalność klienta
    1. Zahashowanie hasła użytkownika
    2. Odszyfrowanie klucza prywatnego
    5. Odszyfrowanie wiadomosci
    4. Zapis

--- WindowsFormsApp1/Form4.Designer.cs
+++ WindowsFormsApp1/Form4.Designer.cs
@@ -31,15 +31,19 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(365, 67);
+            this.button1.Location = new System.Drawing.Point(258, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(72, 29);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Połącz";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -47,31 +51,63 @@
             // 
             this.label4.AutoSize = true;
             this.label4.ForeColor = System.Drawing.Color.Red;
-            this.label4.Location = new System.Drawing.Point(148, 84);
+            this.label4.Location = new System.Drawing.Point(100, 18);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(149, 17);
+            this.label4.Size = new System.Drawing.Size(120, 13);
             this.label4.TabIndex = 8;
             this.label4.Text = "Brak wykrytego serwera";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(29, 84);
+            this.label3.Location = new System.Drawing.Point(11, 18);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(113, 17);
+            this.label3.Size = new System.Drawing.Size(88, 13);
             this.label3.TabIndex = 7;
             this.label3.Text = "Stan połączenia:";
             // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(12, 72);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(318, 20);
+            this.passwordTextBox.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Podaj hasło użytkownika:";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(184, 116);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(146, 33);
+            this.saveButton.TabIndex = 11;
+            this.saveButton.Text = "Zatwierdź  i zapisz plik";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
             // Form4
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(352, 163);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passwordTextBox);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form4";
-            this.Text = "Form4";
+            this.Text = "SuperEncrypter - Client";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,5 +118,8 @@
         private System.Windows.Forms.Button button1;
         public System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button saveButton;
     }
 }
--- WindowsFormsApp1/Form4.cs
+++ WindowsFormsApp1/Form4.cs
@@ -9,11 +9,17 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
 using static AsynchronousClient;
+using System.Security.Cryptography;
+using Microsoft.Win32;
+using System.IO;
 
 namespace WindowsFormsApp1
 {
+
     public partial class Form4 : Form
     {
+        private byte[] _receivedFile = null;
+
         public Form4()
         {
             InitializeComponent();
@@ -29,5 +35,153 @@ namespace WindowsFormsApp1
             }).Start();
 
         }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if(label4.Text == "Brak wykrytego serwera")
+            {
+                MessageBox.Show("Nie wykryto połączenia z serwerem!", "Błąd połączenia!", MessageBoxButtons.OK);
+                return;
+            }
+
+            //Tutaj Kacprowa magia <================================================================================================== ODBIÓR PLIKU =======================
+
+            /*
+             *    W WIADOMOŚCI POTRZEBUJĘ:
+             * 1. Klucz sesyjny
+             * 2. Wektor inicjujący
+             * 3. Tryb szyfrowania
+             * 4. Wiadomość
+             * */
+
+            //tu trzeba wrzucić zaszyfrowany klucz odebrany z serwera
+            byte[] receivedSessionKey = new byte[32];
+
+            //tu trzeba wrzucić zaszyfrowaną wiadomość
+            byte[] receivedMessage = new byte[0];
+
+            //       Dwóch poniższych chyba nie trzeba szyfrować
+
+            //tu trzeba wrzucić wektor inicjujący
+            byte[] receivedIV = new byte[16];
+
+            //tu trzeba wrzucić tryb szyfrowania
+            string cipherMode = "";
+
+
+            /*
+        *      SCENARIUSZ INTERAKCJI
+        *  0. Pobranie całej wiadomości
+        *  1. Zahashowanie hasła użytkownika
+        *  2. Odszyfrowanie klucza prywatnego
+        *  3. Odszyfrowanie klucza sesyjnego
+        *  4. Odszyfrowanie wiadomosci
+        *  5. Zapis 
+        */
+
+            byte[] hashedPassword = HashUserPassword(passwordTextBox.Text);
+            byte[] privateKey = DecryptPrivateKey(hashedPassword);
+            byte[] sessionKey = DecryptSessionKey(receivedSessionKey, privateKey);
+            byte[] message =  DecryptMessage(sessionKey, receivedIV, receivedMessage, cipherMode);
+            ByteArrayToFile(message);
+
+        }
+        private void CreateFirstRegistration()
+        {
+            EncryptPrivateKey(Aes.Create().Key, HashUserPassword("Plagiacik12"));
+        }
+
+        private void EncryptPrivateKey(byte[] privateKey, byte[] hashedPassword)
+        {
+            // wektor inicjujący do zaszyfrowania hasła
+            byte[] IV = new byte[16];
+            for (int i = 0; i < 16; i++)
+                IV[i] = 0;
+
+            // zaszyfrowanie klucza prywatnego za pomocą skrótu hasła
+            byte[] encryptedPrivateKey = new Encoder(hashedPassword, IV).EncryptByCBC(privateKey);
+
+            //zapis zaszyfrowanego klucza prywatnego do rejetru
+            Registry.CurrentUser.CreateSubKey("SuperEncrypter").SetValue("P_KEY", encryptedPrivateKey);
+        }
+
+        private byte[] DecryptPrivateKey(byte[] hashedPassword)
+        {
+            // wektor inicjujący do odszyfrowania hasła
+            byte[] IV = new byte[16];
+            for (int i = 0; i < 16; i++)
+                IV[i] = 0;
+
+            //odczyt zaszyfrowanego klucza prywatnego
+            byte[] encryptedPrivateKey = (byte[])Registry.CurrentUser.OpenSubKey("SuperEncrypter").GetValue("P_KEY");
+
+            return new Encoder(hashedPassword, IV).DecryptByCBC(encryptedPrivateKey);
+        }
+
+        private byte[] HashUserPassword(string plainPassword)
+        {
+            return MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(plainPassword));
+        }
+
+
+        private byte[] DecryptSessionKey(byte[] encryptedSessionKey, byte[] privateKey)
+        {
+            /*
+             *  JAK ODSZYFROWAĆ KLUCZ SESYJNY?
+             *  
+             *  Mam klucz prywatny
+             *  
+             *  I chyba muszę użyć algorytmu RSA do odszyfrowania
+             *  
+             *          ...tylko jeszcze nie wiem jak
+             */
+
+            return new byte[1];
+        }
+
+        private byte[] DecryptMessage(byte[] Key, byte[] IV, byte[] message, string cipherMode)
+        {
+            Encoder enc = new Encoder(Key, IV);
+            byte[] decrypted = null;
+
+            switch (cipherMode)
+            {
+                case "ECB":
+                    decrypted = enc.DecryptByECB(message);
+                    break;
+
+                case "CBC":
+                    decrypted = enc.DecryptByCBC(message);
+                    break;
+
+                case "CFB":
+                    decrypted = enc.DecryptByCFB(message);
+                    break;
+
+                case "OFB":
+                    decrypted = enc.DecryptByOFB(message);
+                    break;
+
+                default:
+                    MessageBox.Show("Niepoprawny tryb szyfrowania!\nPobierz plik jeszcze raz!", "Błąd trybu szyfrowania!", MessageBoxButtons.OK);
+                    break;
+            }
+
+            return decrypted;
+        }
+
+        private void ByteArrayToFile(byte[] array)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "txt files (*.txt)|*.txt|mp3 files (*.mp3)|*.mp3|png files (*.png)|*.png|avi files (*.avi)|*.avi|jpg files (*.jpg)|*.jpg|All files (*.*)|*.*";
+            saveFileDialog.ShowDialog();
+
+            FileStream fs = (System.IO.FileStream)saveFileDialog.OpenFile();
+            fs.Close();
+            File.WriteAllBytes(fs.Name, array);
+        }
+
+
+
     }
 }
